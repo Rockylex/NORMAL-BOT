7fdfcb2d0c59adbd17c62150192efa43c92e4f96
Rockylex <mannbyron65@gmail.com>
2025-01-18 00:03:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
-    PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "Ibrahim Adams",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " Ibrahim Adams",              
+module.exports = { session: process.env.SESSION_ID || EPkkRI6T#0Rv0wfI8-XmSbGFW9I9elnruV1lbEJO4ufXpQl3EI5Q'',
+    PREFIXE: process.env.PREFIX || ",",
+    OWNER_NAME: process.env.OWNER_NAME || "CHAMP TECH",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || " 2349138691641",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
     BOT : process.env.BOT_NAME || 'BMW_MD',
     URL : process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/h2ydge.jpg',
-    MODE: process.env.PUBLIC_MODE || "yes",
+    MODE: process.env.PUBLIC_MODE || "no",
     PM_PERMIT: process.env.PM_PERMIT || 'yes',
     HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
     HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
